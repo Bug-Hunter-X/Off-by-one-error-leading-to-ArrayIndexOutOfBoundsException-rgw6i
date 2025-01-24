aaa573c038e5144100973a7b4d5f829f17624d9a
BugHunter-X <askzenai@gmail.com>
2025-01-24 14:40:05 +0100

    Add Bug.java file

---
+++ Bug.java
@@ -0,0 +1,9 @@
+public class UncommonBug {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i <= arr.length; i++) {
+            arr[i] = i * 2; 
+        }
+        System.out.println(arr[arr.length -1]);
+    }
+}
